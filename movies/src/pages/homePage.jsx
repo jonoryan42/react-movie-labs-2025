9e59603fae7abf0e63bf09345769a62d709eff6a
Hockey Rocky <bigman2401@outlook.com>
2025-10-19 02:36:28 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -24,7 +24,12 @@ const HomePage = (props) => {
     else setGenreFilter(value);
   };
 
-
+    const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
 
   useEffect(() => {
     fetch(
@@ -54,7 +59,7 @@ const HomePage = (props) => {
     />
 
         </Grid>
-        <MovieList movies={displayedMovies} />
+    <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
 
       </Grid>
     </Grid>
